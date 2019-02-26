9e0cf0ae2577a3f559f85e7a664f30c97321b42e
mingshic <13667636243@163.com>
2019-02-26 10:24:03 +0800

    Update url.py

--- send_top/url.py
+++ send_top/url.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
 
-view_url = "http://172.16.3.107:8080/default/ntop.alert.alertInterface.saveMonitorAlert.biz.ext"
-#view_url = "http://111.203.122.61/default/ntop.alert.alertInterface.saveMonitorAlert.biz.ext"
+view_url = ""
+
 
