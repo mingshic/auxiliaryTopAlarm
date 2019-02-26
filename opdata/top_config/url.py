31ce9cd0f7da3bbdcfb7d40ddee2522c287861d2
mingshic <13667636243@163.com>
2019-02-26 10:26:35 +0800

    Update url.py

--- opdata/top_config/url.py
+++ opdata/top_config/url.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 
-config_url = "http://172.16.3.107:8080/default/ntop.alert.alertInterface.queryAlertPeizhi.biz.ext"
-#config_url = "http://111.203.122.61/default/ntop.alert.alertInterface.queryAlertPeizhi.biz.ext"
+config_url = ""
+
 
 
